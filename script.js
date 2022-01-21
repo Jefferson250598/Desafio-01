920c3dc48f30b0e0452d02cc19d45e14fdcd6a81
jefferson250598 <amarantegeraldelli@gmail.com>
2022-01-21 16:28:14 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// Crie uma variável que receba um nome;
+
+let nome = "jefferson Amarante"
+  console.log(nome)
+
+// Crie uma variável que receba um número;
+
+let idade = 23
+ console.log(idade)
+
+// Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+
+let ano = 2022 - idade
+ console.log(ano)
+
+// Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+
+let soma = 2022 + idade
+  console.log(soma)
+
+//Mostre no console a mensagem "Estou mexendo no console.log";
+
+console.log(Estou mexendo no console.log)
+
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console;
+
+let quartaFeira = false
+  console.log(não é quarta feira)
+
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+
+console.log(typeof quartaFeira)
